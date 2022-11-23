6bb6556b9d5928d10a9584679bf63d49523a1980
hoonkim <hoonk9387@gmail.com>
2022-11-22 21:38:05 -0600

    added function as expression to app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 const greeting = "Hello!!!"
+const className = "blue"
+
+function getColor(isRed) {
+  if (isRed) {
+    return "red"
+  } else {
+    return "blue"
+  }
+}
 
 function App() {
   return (
     <div className="App">
       {/* JSX */}
-      <h1 id="header" className="blue">{greeting}{1+1}</h1>
+      <h1 id="header" className={className}>{greeting}{1+1}</h1>
+      <h2>{getColor(true)}{getColor(false)}</h2>
     </div>
   );
 }
